bf4117bce1ed9aef27b893a9a1128242c168ae50
Jeppe-MH <jeppemoenstedhansen@gmail.com>
2024-09-27 10:56:49 +0200

    Update main.cpp

--- src/exercise-0x00/main.cpp
+++ src/exercise-0x00/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Exercise 0x00" << std::endl;
+  double n1, n2 ,n3;
+  cout << "enter 3 numbers: ";
+  cin >> n1 >> n2 >> n3;
+
+if (n1=> n2 && n1=> n3) 
+  cout << "largest number is; " << n1 << endl;
+else if (n2 => n1 && n2 => n3)
+  cout << "largest number is; " << n2 << endl;
+else if (n3 => n1 && n3 => n3)
+  cout "largest number is; " << n3 << endl;
+  
+  
   return 0;
 }
